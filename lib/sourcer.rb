1de6e0de2e9657bb8a8f87fed211f4881544696d
jasonm <jason.p.morrison@gmail.com>
2010-02-15 22:09:17 -0500

    fooling around with OptionParser

--- lib/sourcer.rb
+++ lib/sourcer.rb
@@ -1,20 +1,26 @@
 require 'optparse'
 
 class Sourcer
-  attr_accessor :site, :user_agent
+  attr_accessor :user_agent, :url
 
-  def initialize(stdin, arguments = [])
+  def initialize
     self.user_agent = iphone
 
     options = OptionParser.new
+    options.banner = "Usage: sourcer [options]"
     options.on('-A', '--user-agent') { self.user_agent = a }
-    options.parse!(arguments)
+    options.parse!
 
-    self.site = stdin.gets.chomp
+    # maybe this is a hack, maybe it's the right way to do it, i'm not sure...
+    self.url = options.default_argv.last
   end
 
   def run
-    `curl -A "#{user_agent}" "#{site}"`
+    puts "Scraped #{self.url} as #{self.user_agent} and got:\n#{site}"
+  end
+
+  def site
+    `curl -A "#{user_agent}" "#{url}"`
   end
 
   protected
